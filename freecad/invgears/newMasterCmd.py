1d0cace9bd72a9c8c429ae871bfa20a5caac58aa
MaxiV <81501376+andesfreedesign@users.noreply.github.com>
2025-03-30 00:29:06 -0300

    Update newMasterCmd.py

--- freecad/invgears/newMasterCmd.py
+++ freecad/invgears/newMasterCmd.py
@@ -25,7 +25,7 @@ import FreeCAD as App
 import FreeCADGui as Gui
 import os
 
-from PySide2.QtWidgets import QDialogButtonBox
+from PySide6.QtWidgets import QDialogButtonBox
 from freecad.invgears.featureClasses import MasterGear, SlaveGear, ViewProviderMasterGear, ViewProviderSlaveGear
 
 
